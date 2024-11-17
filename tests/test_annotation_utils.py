1ccc71474b4e9039d00fe4c0a8ca8ff426289dc0
Michael Li <mealted.lemon@gmail.com>
2024-11-17 15:17:38 -0800

    adding get_annotation test cases

--- tests/test_annotation_utils.py
+++ tests/test_annotation_utils.py
@@ -1,6 +1,7 @@
 import pytest
+from pydantic import BaseModel
 
-from pandas_to_pydantic import get_model_columns
+from pandas_to_pydantic import get_annotations, get_model_columns
 
 from .data.parent_child.parent_child_model_columns import model_columns_dict
 from .data.parent_child.parent_child_types import BaseModelErrorModel
@@ -15,3 +16,83 @@ class TestExpandAnnotation:
     def test_base_model_exception(self):
         with pytest.raises(TypeError):
             get_model_columns(BaseModelErrorModel)
+
+
+class TestGetAnnotations:
+    def test_basic_annotations(self):
+        class Book(BaseModel):
+            BookID: int
+            Title: str
+            Genre: str
+            PublishedYear: int
+            AvailableCopies: int
+
+        assert get_annotations(Book) == {
+            "BookID": int,
+            "Title": str,
+            "Genre": str,
+            "PublishedYear": int,
+            "AvailableCopies": int,
+        }
+
+    def test_child_annotations(self):
+        class Author(BaseModel):
+            AuthorID: int
+            AuthorName: str
+            AuthorBirthdate: str
+
+        class Book(BaseModel):
+            BookID: int
+            BookAuthor: Author
+            Title: str
+            Genre: str
+            PublishedYear: int
+            AvailableCopies: int
+
+        assert get_annotations(Book) == {
+            "BookID": int,
+            "BookAuthor": Author,
+            "Title": str,
+            "Genre": str,
+            "PublishedYear": int,
+            "AvailableCopies": int,
+        }
+
+    def test_list_annotations(self):
+        class Book(BaseModel):
+            BookID: int
+            Title: str
+            Genre: str
+            PublishedYear: int
+            AvailableCopies: int
+
+        class Author(BaseModel):
+            AuthorID: int
+            AuthorName: str
+            AuthorBirthdate: str
+            BookList: list[Book]
+
+        assert get_annotations(Author) == {
+            "AuthorID": int,
+            "AuthorName": str,
+            "AuthorBirthdate": str,
+            "BookList": list[Book],
+        }
+
+    def test_inherited_annotations(self):
+        class BaseLibrary(BaseModel):
+            LibraryID: int
+            LibraryName: str
+
+        class InheritedLibrary(BaseLibrary):
+            Location: str
+            EstablishedYear: int
+            BookCollectionSize: int
+
+        assert get_annotations(InheritedLibrary) == {
+            "LibraryID": int,
+            "LibraryName": str,
+            "Location": str,
+            "EstablishedYear": int,
+            "BookCollectionSize": int,
+        }

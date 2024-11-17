24b369f45d1a10147a223149b82c37792b4ccbc4
Michael Li <mealted.lemon@gmail.com>
2024-11-17 15:57:34 -0800

    update test for get_model_columns to be explicit
    Removing parameterization due to being confusing. Using example library models
    instead of generic child and grandchild models

--- tests/test_annotation_utils.py
+++ tests/test_annotation_utils.py
@@ -1,19 +1,120 @@
 import pytest
 from pydantic import BaseModel
 
-from pandas_to_pydantic import get_annotations, get_model_columns
-
-from .data.parent_child.parent_child_model_columns import model_columns_dict
-from .data.parent_child.parent_child_types import BaseModelErrorModel
+from pandas_to_pydantic import ModelColumns, get_annotations, get_model_columns
 
 
-# TODO test different column maps
-class TestExpandAnnotation:
-    @pytest.mark.parametrize("input_model,output_model_columns", model_columns_dict)
-    def test_get_model_columns(self, input_model, output_model_columns):
-        assert get_model_columns(input_model).model_dump() == output_model_columns
+class TestGetModelColumns:
+    def test_basic_model_columns(self):
+        class Book(BaseModel):
+            BookID: int
+            Title: str
+            Genre: str
+            PublishedYear: int
+            AvailableCopies: int
+
+        assert get_model_columns(Book) == ModelColumns(
+            name="Book",
+            id_column=None,
+            base_columns=["BookID", "Title", "Genre", "PublishedYear", "AvailableCopies"],
+            list_columns=[],
+            child_columns=[],
+        )
+
+    def test_child_model_columns(self):
+        class Author(BaseModel):
+            AuthorID: int
+            AuthorName: str
+            AuthorBirthdate: str
+
+        class Book(BaseModel):
+            BookID: int
+            BookAuthor: Author
+            Title: str
+            Genre: str
+            PublishedYear: int
+            AvailableCopies: int
+
+        assert get_model_columns(Book) == ModelColumns(
+            name="Book",
+            id_column=None,
+            base_columns=["BookID", "Title", "Genre", "PublishedYear", "AvailableCopies"],
+            list_columns=[],
+            child_columns=[
+                ModelColumns(
+                    name="BookAuthor",
+                    id_column=None,
+                    base_columns=["AuthorID", "AuthorName", "AuthorBirthdate"],
+                    list_columns=[],
+                    child_columns=[],
+                )
+            ],
+        )
+
+    def test_list_model_columns(self):
+        class Book(BaseModel):
+            BookID: int
+            Title: str
+            Genre: str
+            PublishedYear: int
+            AvailableCopies: int
+
+        class Author(BaseModel):
+            AuthorID: int
+            AuthorName: str
+            AuthorBirthdate: str
+            BookList: list[Book]
+
+        assert get_model_columns(Author, {"Author": "AuthorID", "BookList": "BookID"}) == ModelColumns(
+            name="Author",
+            id_column="AuthorID",
+            base_columns=["AuthorID", "AuthorName", "AuthorBirthdate"],
+            list_columns=[
+                ModelColumns(
+                    name="BookList",
+                    id_column="BookID",
+                    base_columns=["BookID", "Title", "Genre", "PublishedYear", "AvailableCopies"],
+                    list_columns=[],
+                    child_columns=[],
+                )
+            ],
+            child_columns=[],
+        )
+
+    def test_inherited_model_columns(self):
+        class BaseLibrary(BaseModel):
+            LibraryID: int
+            LibraryName: str
+
+        class InheritedLibrary(BaseLibrary):
+            Location: str
+            EstablishedYear: int
+            BookCollectionSize: int
+
+        assert get_model_columns(InheritedLibrary) == ModelColumns(
+            name="InheritedLibrary",
+            id_column=None,
+            base_columns=["Location", "EstablishedYear", "BookCollectionSize", "LibraryID", "LibraryName"],
+            list_columns=[],
+            child_columns=[],
+        )
+
+    def test_empty_model_columns(self):
+        class EmptyModel(BaseModel):
+            pass
+
+        assert get_model_columns(EmptyModel) == ModelColumns(
+            name="EmptyModel",
+            id_column=None,
+            base_columns=[],
+            list_columns=[],
+            child_columns=[],
+        )
 
     def test_base_model_exception(self):
+        class BaseModelErrorModel(BaseModel):
+            error_list: list[str]
+
         with pytest.raises(TypeError):
             get_model_columns(BaseModelErrorModel)
 
